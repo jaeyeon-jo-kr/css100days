8c8382afd09ac8f8b159d247a7a6589f1c745c40
Jaeyeon Jo <jojaeyeon.kr@gmail.com>
2023-04-17 21:47:43 +0900

    Add day9

--- src/css100days/day9.cljs
+++ src/css100days/day9.cljs
@@ -9,7 +9,7 @@
 
 (def moon-style
   [:.moon {:position "absolute"
-           :top "-120px"
+           :top "-120px" 
            :left "-120px"
            :width "67px"
            :height "67px"
@@ -29,7 +29,7 @@
             :border-radius "2px"
             :box-shadow "4px 8px 16px 0 rgba(0,0,0,0.1)"
             :overflow "hidden"
-            :background "#011628"
+            :background "#1A2238"
             :color "#333"
             :font-family "'Open Sans', Helvetica, sans-serif"
             :-webkit-font-smoothing "antialiased"
@@ -40,6 +40,31 @@
              :top "50%"
              :left "50%"
              :transform "translate(-50%,-50%)"}])
+
+(def drop-style
+  [:.drop {:position "absolute"
+           :left "18px"
+           :bottom "-30px"
+           :width "8px"
+           :height "8px"
+           :border-radius "50%"
+           :background "white"
+           :animation "drop 2s linear 0.4s infinite"
+           :--webkit-animation "drop 0.2s linear 0.4s infinite"
+           :transform-origin "50% 100%"
+           :animation-fill-mode "both"}
+   [:&:before
+    {:position "absolute"
+     :display "block"
+     :background "#028192"
+     :top "10px"
+     :left "40px"}]
+   [:&:after 
+    {:position "absolute"
+     :display "block"
+     :background "blue"
+     :top "10px"
+     :left "30px"}]])
 
 (def hill-style
   [:.hill-bg 
@@ -74,10 +99,76 @@
     :background "#028192"
     :border-radius "90%"}])
 
-(def drop
-  [:.drop 
-   {:position }])
+(def temperature-style
+  [:.temperature 
+   {:position "relative"
+    :z-index 20
+    :left "20px"
+    :top "30px"
+    :font-size "35px"}])
 
+(def wind-style
+  [:.wind 
+   {:position "relative"
+    :top "-10px"
+    :left "80px"}])
+
+(def humidity-style
+  [:.humidity
+   {:position "relative"
+    :top "-10px"
+    :left "80px"}])
+
+(def daily-style
+  [:.daily
+   {:position "relative"
+    :top "-57%"
+    :left "65%"}])
+
+(def front-style
+  [:.front 
+   {:position "absolute"
+    :z-index 10
+    :width "400px"
+    :height "90px"
+    :left "0px"
+    :bottom "0px"
+    :background "#fff"}
+   temperature-style
+   wind-style
+   humidity-style
+   daily-style])
+
+(def drop-keyframe
+  (str "\n@keyframes drop { 
+  0% {
+    transform: translate3d(40px, -400px, 0) scaleX(1) scaleY(1) rotate(17deg);
+  }
+  85% {
+    transform: translate3d(0, 0, 0) scaleX(1) scaleY(1) rotate(17deg);
+  }
+  100% {
+    transform: translate3d(0, 0, 0) scaleX(3) scaleY(0) rotate(0deg);
+  }
+}"))
+
+  
+
+(defn styles
+  []
+  (-> (->> [frame-style
+            center-style
+            moon-style
+            hill-style
+            hill-2-style
+            hill-3-style
+            front-style
+            drop-style]
+           (apply css))
+      (str " " drop-keyframe)))
+
+(def drop-comp
+  [:div {:class "drop"}])
 
 (def moon 
   [:div {:class "moon"}])
@@ -91,14 +182,35 @@
 (def hill-3-background
   [:div {:class "hill-3-bg"}])
 
-(defn styles
-  []
-  (css frame-style 
-       center-style 
-       moon-style 
-       hill-style 
-       hill-2-style
-       hill-3-style))
+
+(def humidity
+  [:div {:class "humidity"}
+   "Humidity: 87%"])
+
+(def wind
+  [:div {:class "wind"}
+   "Wind: E 7 km/h"])
+
+(def temperature
+  [:div {:class "temperature"}
+   "12°"])
+
+(def daily
+  [:table {:class "daily"}
+   [:tr
+    [:td "TUE"]
+    [:td "21° / 9°"]]
+   [:tr
+    [:td "WED"]
+    [:td "23° / 10°"]]])
+
+(def front
+  [:div {:class "front"}
+   temperature
+   wind
+   humidity
+   daily])
+
 
 (defn framework
   []
@@ -108,5 +220,7 @@
       moon
       hill-background
       hill-2-background
-      hill-3-background]]))
+      hill-3-background
+      drop-comp]
+     front]))
 
